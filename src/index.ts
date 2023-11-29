ec22ca8949f2ae7617e3063a9f513fda76f03dc3
= <=>
2023-11-29 12:42:46 +0800

    test: Test create entry and delete

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono"
 import { serve } from "@hono/node-server"
 import { font } from "ascii-art"
+import { studentEntryRoute } from "./routes/studentEntry"
 
 const app = new Hono()
+
+app.route("/entry", studentEntryRoute)
+
 serve(app, () => {
     font("Attendance App", "Doom").toPromise().then((data) => {
         console.log(data);
