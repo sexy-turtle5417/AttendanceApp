1ad9f2b78d9a989840e748176d10a0103f00a285
= <=>
2023-11-29 11:02:57 +0800

    feat: Start new branch to fix database schema

--- src/routes/StudentInRoute.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { Hono } from "hono";
-
-export const StudentInRoute = new Hono()
-const prisma = new PrismaClient()
-
-StudentInRoute.post("/create")
